c4d3b621c3d2747cdf7c862a9cbdf7d6a228e071
Tuan Luong <tuanluong.it@gmail.com>
2020-08-08 16:56:22 +0700

    Update wbservice

--- app/src/main/java/com/wosmart/sdkdemo/services/WbService.java
+++ app/src/main/java/com/wosmart/sdkdemo/services/WbService.java
@@ -1,21 +1,33 @@
 package com.wosmart.sdkdemo.services;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.wosmart.sdkdemo.manager.WbManager;
 
-public class WbService extends IntentService {
-    public WbService() {
-        super("WbService");
+public class WbService extends Service {
+    private static final String TAG = "WbService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG, "Service started");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        Log.e("WbService", "Service running");
-        WbManager.getInstance(getApplicationContext()).start();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        WbManager.getInstance(this).start();
+        return START_STICKY;
     }
 }
