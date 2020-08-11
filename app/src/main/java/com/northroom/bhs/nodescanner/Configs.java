5afa6d6fb8688ab84115362db1de7399046ada15
Tuan Luong <tuanluong.it@gmail.com>
2020-08-11 13:16:58 +0700

    update wifi config

--- app/src/main/java/com/northroom/bhs/nodescanner/Configs.java
+++ app/src/main/java/com/northroom/bhs/nodescanner/Configs.java
@@ -1,6 +1,9 @@
 package com.northroom.bhs.nodescanner;
 
 public class Configs {
-    public static final String WIFI_SSID = "Betram";
-    public static final String WIFI_PASSWORD = "B3tr@m321!";
+//    public static final String WIFI_SSID = "Betram";
+//    public static final String WIFI_PASSWORD = "B3tr@m321!";
+
+    public static final String WIFI_SSID = "RustlethyJimmes";
+    public static final String WIFI_PASSWORD = "Pandaza2806!";
 }
